49ffee26ef45f3988a2c262842138c267ccfb280
Guilherme-Endrigo <endrigo.guilherme@hotmail.com>
2024-05-25 23:29:14 -0300

    git commit -m ":sparkles: feat: GameTimer

---
+++ ADayWithMorte.Shared/Sistema/Timer/GameTimer.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+
+namespace ADayWithMorte.Shared.Sistema.Timer
+{
+    public class GameTimer
+    {
+        private Stopwatch stopwatch;
+        private TimeSpan initialTime;
+
+        public GameTimer(DateTime? startTime = null)
+        {
+            stopwatch = new Stopwatch();
+            initialTime = startTime.HasValue ? DateTime.Now - startTime.Value : TimeSpan.Zero;
+        }
+
+        public void Start()
+        {
+            stopwatch.Start();
+
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    TimeSpan ts = stopwatch.Elapsed + initialTime;
+                    Thread.Sleep(1000);
+                }
+            }).Start();
+        }
+
+        public TimeSpan GetElapsedTime()
+        {
+            return stopwatch.Elapsed;
+        }
+    }
+}
